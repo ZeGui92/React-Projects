6afe8b88d14a99f094110307a54a1f84ec2138d0
Jose <jgaugustinho@gmail.com>
2025-01-27 13:47:08 -0300

    creation of a recipe page and its programmatic routes.

--- 04-recipe/src/components/Card.jsx
+++ 04-recipe/src/components/Card.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function Card( { recipe } ) {
+    const navigate = useNavigate();
+
+    const navigateToRecipePage = () => {
+        navigate( `/recipe/${ recipe.id }` ); // or ("recipe/" + recipe.id)
+    }
+
     return (
-        <div className="card">
+        <div className="card" onClick={ navigateToRecipePage }>
             <img src={ recipe.image } alt="" />
             <div className="card-content">
                 <h3>{ recipe.name }</h3>
--- 04-recipe/src/main.jsx
+++ 04-recipe/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import AboutPage from './pages/AboutPage.jsx'
 import HomePage from './pages/HomePage.jsx'
+import RecipePage from './pages/RecipePage.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={ <App /> }>
       <Route path='/' element={ <HomePage /> } />
       <Route path='/about' element={ <AboutPage /> } />
+      <Route path='/recipe/:id' element={ <RecipePage /> } />
     </Route>
   )
 );
