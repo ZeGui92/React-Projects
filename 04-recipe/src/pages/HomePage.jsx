150cab365e21b27d816d7053ba1320605b1f17ab
Jose <jgaugustinho@gmail.com>
2025-01-29 17:28:40 -0300

    use free Rapid API API to fetch recipe data.

--- 04-recipe/src/pages/HomePage.jsx
+++ 04-recipe/src/pages/HomePage.jsx
@@ -1,7 +1,34 @@
 import CardList from "../components/CardList";
 import Header from "../components/Header";
+import axios from "axios";
+import { useEffect } from "react";
+import myKey from "../config";
+
+const options = {
+  method: 'GET',
+  url: 'https://tasty.p.rapidapi.com/recipes/list',
+  params: {
+    from: '0',
+    size: '20'
+  },
+  headers: {
+    'x-rapidapi-key': myKey,
+    'x-rapidapi-host': 'tasty.p.rapidapi.com'
+  }
+};
 
 export default function HomePage() {
+  const fetchRecipes = async () => {
+    try {
+      const response = await axios.request( options );
+      console.log( response.data );
+    } catch ( error ) {
+      console.error( error );
+    }
+  }
+
+  useEffect( () => { fetchRecipes() }, [] );
+
   return (
     <>
       <Header />
